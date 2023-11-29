8e60fe5eea92cb5225a1c1138b10f66511bcabb0
robin <robin>
2023-11-29 10:34:42 +0000

    sample

--- warning.py
+++ warning.py
@@ -1,5 +1,7 @@
+import subprocess
+
 def app(enciron, start_response):
-    subprocess.call(["play","/data/data/com.termux/files/home/storage/downloads/clips/large_drink.wav"])
+    subprocess.call(["play","large_drink.wav"])
     status = "200 OK"
     response_headers = [
         ("Content-type", "text/plain"),
@@ -7,3 +9,4 @@ def app(enciron, start_response):
     ]
     start_response(status, response_headers)
     return iter([b"hi"])
+
